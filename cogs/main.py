e5a27c5c4f8a7f194145520381e1d2a95da620be
Forestcat <forest_cat@dismail.de>
2024-07-27 02:00:19 +0200

    switch to pycords ping information due to permissionerror on linux

--- cogs/main.py
+++ cogs/main.py
@@ -1,9 +1,7 @@
 from discord.commands import slash_command
 from discord.ext import commands
-import icmplib
 import sys
 import os
-
 
 class Main(commands.Cog):
     def __init__(self, bot):
@@ -15,9 +13,6 @@ class Main(commands.Cog):
 
     config = read_config()
 
-    async def host_ping(self, host):
-        ping = icmplib.ping(address=host, count=4, privileged=False)
-        return int(round(ping.avg_rtt))
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -26,11 +21,9 @@ class Main(commands.Cog):
 
     @slash_command(name='ping',
                    guild_ids=config["GUILD_IDS"],
-                   description="Shows you the bots latency all the used services")
+                   description="Shows you the bots latency to the discord api")
     async def ping(self, ctx):
-        msg = await ctx.respond("-# *pinging* <a:loadingemoji:1266532115634323507>")
-        await msg.edit_original_response(content=f"""Discord.com: `{await self.host_ping('discord.com')}`ms\nYouTube.de: `{await self.host_ping('youtube.de')}`ms\nYouTube.com: `{await self.host_ping('youtube.com')}`ms""")
-
+        await ctx.respond(f"My Latency is: `{int(round(self.bot.latency*60, 0))}ms`")
 
 def setup(bot):
     bot.add_cog(Main(bot))
